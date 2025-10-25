6e12d7b95a6eec5933127eb6682168a25b49c30c
Kai-Griffin <mendozailadio@gmal.com>
2025-10-25 16:33:03 +0800

    Set up BackEnd

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+// create express app
+const app = express();
+
+
+// middleware to parse JSON requests
+app.use(express.json());
+
+const port = 3000;
+
+try {
+    app.listen(port,() => {
+        console.log('Listening port 3000...');
+    });   
+} catch (e) {
+    console.log(e);
+}
+
+app.get('/Iladio',async (request,response) => {
+    response.status(200).json({message: 'Hello Im Young Dang!!'});
+});
+
+// ipconfig, example 192.168.194.88:3000 to access your workmate through IP address
