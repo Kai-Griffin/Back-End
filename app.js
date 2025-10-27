396bbaa2c7dafdc85b2faf8c35c3592173d9ae16
agminyong <ninojosephnino94@gmail.com>
2025-10-27 19:56:54 +0800

    Set Up Back-End

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ try {
     console.log(e);
 }
 
-app.get('/Agquiz',async (request,response) => {
-    response.status(200).json({message: 'Hello Im Aqguiz!!'});
+app.get('/Iladio',async (request,response) => {
+    response.status(200).json({message: 'Hello Im Young Dang!!'});
 });
 
 // ipconfig, example 192.168.194.88:3000 to access your workmate through IP address
