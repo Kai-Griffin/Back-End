4fc3c0fda0e8bd857a06a80a621bf7b83cd5ebd5
Kai-Griffin <mendozailadio@gmail.com>
2025-10-27 20:33:26 +0800

    Setup BackEnd

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ try {
     console.log(e);
 }
 
-app.get('/Agquiz',async (request,response) => {
-    response.status(200).json({message: 'Hello Im Agquiz!!'});
+app.get('/Iladio',async (request,response) => {
+    response.status(200).json({message: 'Hello Im  Stunna !!'});
 });
 
 // ipconfig, example 192.168.194.88:3000 to access your workmate through IP address
